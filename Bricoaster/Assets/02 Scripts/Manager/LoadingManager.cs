e0f2b3069b0307e598ba2a31a2087b7196396e63
Johyechan <98877897+Johyechan@users.noreply.github.com>
2024-08-21 23:45:07 +0900

    Change Scene Change method

--- Bricoaster/Assets/02 Scripts/Manager/LoadingManager.cs
+++ Bricoaster/Assets/02 Scripts/Manager/LoadingManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingManager : MonoBehaviour
 {
     void Start()
     {
-        
+        StartCoroutine(ChangeScene());
     }
 
-    void Update()
+    private IEnumerator ChangeScene()
     {
-        
+        // 여기 이제 Addressable로 데이터를 다 로드 하면 넘어가게 ㄱㄱ
+        // 내일 위에 있는 거 하고 빌드되는 모습 Giude에서 보이게 하기
+        yield return null;
+        KeepScene keep = GameObject.Find("KeepScene").GetComponent<KeepScene>();
+        SceneManager.LoadScene(keep.SceneName);
     }
 }
